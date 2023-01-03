a53fbc44160a0c5083b1b286c51a1b1ad4d41361
xarey0629 <xarey0629@gmail.com>
2023-01-03 19:34:48 +0800

    Subscription is allowed.

--- server/resolvers/Mutation.js
+++ server/resolvers/Mutation.js
@@ -1,8 +1,8 @@
 // import { LaserCutterModel } from "../database/mongo/models/machine";
 const Model = require("../database/mongo/models/machine");
 const { ReserveLaserModel } = require("../database/mongo/models/reservation");
-const { PubSub } = require('graphql-subscriptions');
-const pubsub = new PubSub();
+// const { PubSub } = require('graphql-subscriptions');
+// const pubsub = new PubSub();
 
 const Mutation = {
   createMachine: async (
@@ -26,83 +26,91 @@ const Mutation = {
   },
   createLaserCutter: async (
     parents,
-    { info: { id, status, duration, user, completeTime } },
+    { info: { id, status, duration, user, completeTime } }
+  ) =>
     // { pubsub }
-  ) => {
-    let laserCutter = await Model.LaserCutterModel.findOne({ id });
-    if (!laserCutter){
-      console.log('LaserCutterModel不存在 -> 建立LaserCutterModel');
-      // console.log({ id, status, duration, user, completeTime });
-      laserCutter = await new Model.LaserCutterModel({ id: id, status: status, duration: duration, user: user, completeTime: completeTime }).save();
-    }
-    else{
-      console.log("Find Current LaserCutter:", laserCutter.id);
-    }
+    {
+      let laserCutter = await Model.LaserCutterModel.findOne({ id });
+      if (!laserCutter) {
+        console.log("LaserCutterModel不存在 -> 建立LaserCutterModel");
+        // console.log({ id, status, duration, user, completeTime });
+        laserCutter = await new Model.LaserCutterModel({
+          id: id,
+          status: status,
+          duration: duration,
+          user: user,
+          completeTime: completeTime,
+        }).save();
+      } else {
+        console.log("Find Current LaserCutter:", laserCutter.id);
+      }
 
-    console.log('Validation of LaserCutter:', laserCutter);
-    return laserCutter;
-  },
+      console.log("Validation of LaserCutter:", laserCutter);
+      return laserCutter;
+    },
 
   updateLaserCutter: async (
     parents,
     { info: { id, status, duration, user, completeTime } },
-    // { pubsub }
+    { pubsub }
   ) => {
     let laserCutter = await Model.LaserCutterModel.findOneAndUpdate(
       { id },
-      { $set: {
+      {
+        $set: {
           status,
           duration,
           user,
           completeTime,
-        }
+        },
       },
-      {new: true}
+      { new: true }
     );
 
-    if (!laserCutter){
-      console.log('Error LaserCutterModel不存在');
-    }
-    else{
+    if (!laserCutter) {
+      console.log("Error LaserCutterModel不存在");
+    } else {
       console.log("Update Current LaserCutter:", laserCutter.id);
     }
 
     console.log("Validation of LaserCutter:", laserCutter);
-    console.log(pubsub.publish('LaserCutterInfo', { newInfo: laserCutter }));
-    pubsub.publish('LaserCutterInfo', { LaserCutterInfo: laserCutter });
+    console.log(pubsub.publish("LaserCutterInfo", { newInfo: laserCutter }));
+    pubsub.publish("LaserCutterInfo", { LaserCutterInfo: laserCutter });
     return laserCutter;
   },
 
   // for reserving a laser cutter
   createLaserReserve: async (
     parents,
-    { info: { teamId, material, thickness } },
-    { pubsub }
-  ) => {
-    // 曾經預約過-> 更新reserveStatus
-    let reserveLaser = await ReserveLaserModel.findOneAndUpdate(
-      { teamId },
-      { material, thickness, reserveStatus: 1 },
-      { new: true }
-    );
-
-    // 首次預約-> 創立預約紀錄
-    if (!reserveLaser) {
-      reserveLaser = await new ReserveLaserModel({
-        teamId,
-        material,
-        thickness,
-      }).save();
-
-      console.log(
-        "Create new Reservation record of LaserCutter:",
-        reserveLaser
+    { info: { teamId, material, thickness } }
+  ) =>
+    // { pubsub }
+    {
+      // 曾經預約過-> 更新reserveStatus
+      let reserveLaser = await ReserveLaserModel.findOneAndUpdate(
+        { teamId },
+        { material, thickness, reserveStatus: 1 },
+        { new: true }
       );
-    } else console.log("Find Reservation record of LaserCutter:", reserveLaser);
 
-    // console.log("Validation of LaserCutter:", reserveLaser);
-    return reserveLaser;
-  },
+      // 首次預約-> 創立預約紀錄
+      if (!reserveLaser) {
+        reserveLaser = await new ReserveLaserModel({
+          teamId,
+          material,
+          thickness,
+        }).save();
+
+        console.log(
+          "Create new Reservation record of LaserCutter:",
+          reserveLaser
+        );
+      } else
+        console.log("Find Reservation record of LaserCutter:", reserveLaser);
+
+      // console.log("Validation of LaserCutter:", reserveLaser);
+      return reserveLaser;
+    },
 
   // for canceling a laser cutter
   cancelLaserReserve: async (parents, { teamId }, { pubsub }) => {
--- server/resolvers/Subscription.js
+++ server/resolvers/Subscription.js
@@ -12,10 +12,10 @@ const Subscription = {
   // 因為無法測試所以高機率我在亂寫QQ
   // err msg from apollo sandbox: "server must support graphql-ws or subscriptions-transport-ws protocol"
   LaserCutterInfo: {
-    subscribe: (parent, args, {pubsub}, info) => {
+    subscribe: (parent, args, { pubsub }, info) => {
       // console.log(context);
-      console.log(pubsub.asyncIterator('LaserCutterInfo'));
-      return pubsub.asyncIterator('LaserCutterInfo');
+      console.log(pubsub.asyncIterator("LaserCutterInfo"));
+      return pubsub.asyncIterator("LaserCutterInfo");
     },
   },
 

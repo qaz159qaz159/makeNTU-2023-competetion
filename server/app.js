66eca9914b82e253ac870736d182c07dc4d7d540
xarey0629 <xarey0629@gmail.com>
2023-01-03 13:16:24 +0800

    Reconstruct WsServer, subscription is allowed now

--- server/app.js
+++ server/app.js
@@ -15,6 +15,9 @@ const fs = require("fs");
 const ApolloServerPluginDrainHttpServer =
   require("@apollo/server/plugin/drainHttpServer").ApolloServerPluginDrainHttpServer;
 const http = require("http");
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const Subscription = require("./resolvers/Subscription");
@@ -43,22 +46,52 @@ db.once("open", async () => {
     console.log("Trust proxy is on");
     app.set("trust proxy", 1);
   }
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
+
+  const schema = makeExecutableSchema({
     typeDefs: fs.readFileSync("./server/schema.graphql", "utf-8"),
     resolvers: {
       // add other resolvers
       Query,
       Mutation,
       Subscription,
-    },
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    }, 
+  });
+
+  const httpServer = http.createServer(app);
+  // Creating the WebSocket server
+  const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/graphql',
+  });
+
+  // Hand in the schema we just created and have the
+  // WebSocketServer start listening.
+  const serverCleanup = useServer({ schema }, wsServer);
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
     context: async (input) => {
       console.log("input = ", input);
       const req = input.req.headers;
       return { req };
     },
   });
+
+
   await server.start();
 
   app.use(logger("dev"));
@@ -67,7 +100,10 @@ db.once("open", async () => {
   app.use("/api", apiRouter);
   app.use("/graphql", cors(), json(), expressMiddleware(server));
 
-  app.listen(port, () =>
+  // app.listen(port, () =>
+  //   console.log(`App listening at http://localhost:${port}`)
+  // );
+  httpServer.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
   );
 });

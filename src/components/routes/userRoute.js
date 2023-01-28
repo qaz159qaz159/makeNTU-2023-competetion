8928cf1b46a10f576ddd1cc6c4cb38deac8855a7
mhhsu <qaz159qaz159@gmail.com>
2023-01-28 19:54:39 +0800

    Sync

--- src/components/routes/userRoute.js
+++ src/components/routes/userRoute.js
@@ -12,11 +12,7 @@ export default function UserRoute({ children, path }) {
       path={path}
       render={() => {
         if (!isLogin) return <Redirect to="/login" />;
-<<<<<<<< HEAD:src/components/routes/adminRoute.js
         if (authority === 0) return <Redirect to="/" />;
-========
-        if (authority !== 0) return <Redirect to="/" />;
->>>>>>>> upstream/main:src/components/routes/userRoute.js
         return children;
       }}
     />
